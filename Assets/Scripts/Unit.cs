09a54c7806a5a49cb2057183d1227c523298d24a
WizzardMaker <wizzardmaking@gmail.com>
2016-04-16 19:21:01 +0200

    19:20

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -8,19 +8,24 @@ public class Unit : MonoBehaviour {
 	public float speed;
 
 	NavMeshAgent navA;
+	int goal;
 
 	// Use this for initialization
 	void Start () {
 		navA = GetComponent<NavMeshAgent>();
 		navA.speed = speed;
-
+		goal = Random.Range(0, poi.Length);
+		navA.SetDestination(poi[goal]);
 		GetComponentsInChildren<MeshRenderer>()[1].material.color = new Color32((byte)Random.Range(0, 255), (byte)Random.Range(0, 255), (byte)Random.Range(0, 255), 1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Vector3.Distance(transform.position,navA.pathEndPosition) < 2)
-			navA.destination = (poi[0]);
+
+		if (Vector3.Distance(transform.position, poi[goal]) < 2) {
+			goal = Random.Range(0, poi.Length);
+			navA.SetDestination(poi[goal]);
+		}
 		
 	}
 }
